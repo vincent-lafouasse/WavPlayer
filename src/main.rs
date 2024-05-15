fda1ad4996e9fd71d827ade0518efc69deb40339
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-15 14:03:26 +0200

    init output device

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#![allow(unused_variables)]
+
+use cpal::Host;
+use cpal::traits::HostTrait;
+
 fn main() {
+    let host: Host = cpal::default_host();
+    let device = host.default_output_device().expect("no output device available");
+
     println!("Hello, world!");
 }
