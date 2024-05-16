b14ac0d103e60f46db9e2cc277e727758fa36853
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-16 11:47:22 +0200

    open file

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,19 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
+use std::fs::File;
+use std::io::BufReader;
+
 use rodio::OutputStream;
+use rodio::OutputStreamHandle;
 
 const SAMPLE_RATE: u32 = 44_100;
 
+const MP3_PATH: &str = "assets/pra_machucar_meu_coracao.mp3";
+
 fn main() {
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let (_stream, stream_handle): (OutputStream, OutputStreamHandle) =
+        OutputStream::try_default().expect("couldnt open a stream");
+
+    let file = BufReader::new(File::open(MP3_PATH).expect("couldnt open audio file"));
 }
