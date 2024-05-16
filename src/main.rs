e243577e79139876586abf16dab8c0aa3bd30721
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-16 13:41:56 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,12 @@ use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::Data;
 use cpal::Device;
 use cpal::Host;
-use cpal::StreamConfig;
-
-const SAMPLE_RATE: u32 = 44_100;
-//const SAMPLE_FORMAT: cpal::SampleFormat = cpal::SampleFormat::F32;
+use cpal::SupportedStreamConfig;
 
 fn main() {
-    let host: Host = cpal::default_host();
-    let device: Device = host
-        .default_output_device()
-        .expect("no output device available");
-
-    let stream_config = StreamConfig {
-        channels: 1,
-        sample_rate: cpal::SampleRate(SAMPLE_RATE),
-        buffer_size: cpal::BufferSize::Default,
-    };
-
+    let (_host, device, config) = setup_device().unwrap();
     let stream = device.build_output_stream(
-        &stream_config,
+        &config.into(),
         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
             // react to stream events and read or write stream data here.
         },
@@ -33,6 +20,21 @@ fn main() {
         },
         None, // None=blocking, Some(Duration)=timeout
     );
+}
+
+fn setup_device() -> Result<(Host, Device, SupportedStreamConfig), &'static str> {
+    let host: Host = cpal::default_host();
+    let device: Device = match host.default_output_device() {
+        Some(device) => device,
+        None => return Err("no output device available"),
+    };
+
+    let stream_config: SupportedStreamConfig = match device.default_output_config() {
+        Ok(config) => config,
+        Err(_) => return Err("couldnt find default stream config"),
+    };
+
+    Ok((host, device, stream_config))
 }
 
 fn print_type_of<T>(_: &T) {
